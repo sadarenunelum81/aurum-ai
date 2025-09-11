3772b805b9c58de81310d4bc1e7037575dfa7bf7
Nex Battle <nexbattlehelp@gmail.com>
2025-09-11 11:08:02 +0000

    -----
    Error: Error [TurbopackInternalError]: Next.js package not found

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -48,4 +47,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
